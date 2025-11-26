e2831dbada944d64feefd38e5f36b07fd5fa9011
kimhyerim <kimhr4@visang.com>
2025-11-26 23:27:57 +0900

    Feat. route 추가

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,17 @@
-import Three from "@/component/three/Three";
+import Link from "next/link";
+
+import Three from "@/components/three/Three";
+
 import styles from "./page.module.scss";
+
 export default function Home() {
   return (
     <div className={styles.page_wrap}>
+      <h1>메인화면</h1>
       <Three />
+      <Link href="/product/1" className={styles.apply_button}>
+        <span>견적 신청하기</span>
+      </Link>
     </div>
   );
 }
---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductDetailPage() {
+  return (
+    <main>
+      <h1>제품 상세 페이지</h1>
+      <span>
+        여기에 뭐가 있어야 하는거지...
+      </span>
+    </main>
+  );
+}
