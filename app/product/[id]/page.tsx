efc2969b96b312c558009819ef6a9d1bb5b92e52
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-12-04 00:29:21 +0900

    Feat. 드롭다운 추가

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -4,6 +4,29 @@ import Three from "@/components/three/Three";
 import styles from "./page.module.scss";
 
 export default function ProductDetailPage() {
+  const items = [
+    {
+      label: "멀티탭",
+      value: "multi_tab",
+    },
+    {
+      label: "6.35mm 캐이블 패널 마운트",
+      value: "635_cable_panel_mount",
+    },
+    {
+      label: "적층형 구조",
+      value: "layered_structure",
+    },
+    {
+      label: "55파이 케이블 홀더",
+      value: "55_cable_holder",
+    },
+    {
+      label: "쇼바",
+      value: "showba",
+    },
+  ];
+
   return (
     <main className={styles.page_wrap}>
       <article className={styles.article_wrap}>
@@ -13,21 +36,15 @@ export default function ProductDetailPage() {
         <form className={styles.form_wrap}>
           <h1>폐달보드 Pro-M3</h1>
 
-          <div>
+          <div className={styles.form_item}>
             <label>추가 부품</label>
-            {/* <select>
-              <option value="multi_tab">멀티탭</option>
-              <option value="cable_holder">캐이블 홀더</option>
-              <option value="gas_spring">가스 스프링</option>
-              <option value="slide_catch">슬라이드 빗장</option>
-              <option value="cable_mount">캐이블 마운트</option>
-              <option value="635_cable_panel_mount">
-                6.35mm 캐이블 패널 마운트
-              </option>
-            </select> */}
-            <Dropdown />
+            <Dropdown
+              items={items}
+              placeholder="선택해 주세요"
+              style={{ flexGrow: 1 }}
+            />
           </div>
-          <div>
+          <div className={styles.form_item}>
             <label>수량</label>
             <input type="number" min="1" max="100" />
           </div>
--- components/dropdown/Dropdown.tsx
+++ components/dropdown/Dropdown.tsx
@@ -1,18 +1,51 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import ArrowIcon from "@/assets/ArrowIcon";
 
 import styles from "./Dropdown.module.scss";
 
-export default function Dropdown() {
+export default function Dropdown({
+  items = [],
+  placeholder = "선택해 주세요",
+  value = "",
+  onChange = () => {},
+  style = {},
+}: {
+  items?: {
+    label: string;
+    value: string;
+  }[];
+  placeholder?: string;
+  value?: string;
+  onChange?: (value: string) => void;
+  style?: React.CSSProperties;
+}) {
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const [isOpen, setIsOpen] = useState(false);
 
+  /* 드롭다운의 바깥을 클릭하면 드롭다운 닫음 */
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div ref={dropdownRef} className={styles.dropdown_wrap}>
+    <div ref={dropdownRef} className={styles.dropdown_wrap} style={style}>
       <button
         type="button"
         className={styles.dropdown_button}
@@ -20,9 +53,22 @@ export default function Dropdown() {
           setIsOpen((prev) => !prev);
         }}
       >
-        <span>드롭다운</span>
-        <ArrowIcon width={20} height={20} rotate={isOpen ? 90 : 270} />
+        <span>{value || placeholder}</span>
+        <ArrowIcon width={20} height={20} rotate={isOpen ? 270 : 90} />
       </button>
+      {items.length > 0 && (
+        <ul
+          className={`${styles.dropdown_menu} ${
+            isOpen ? styles.dropdown_menu_open : styles.dropdown_menu_closed
+          }`}
+        >
+          {items.map((item) => (
+            <li className={styles.dropdown_item} key={item.value}>
+              <button type="button">{item.label}</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
