1b595a399af4a2469bf0451a6cc1468a4451eeaa
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-12-17 16:20:33 +0900

    Feat. 제품 카테고리 선택 시 로딩 UI 추가

--- app/[productId]/list/page.tsx
+++ app/[productId]/list/page.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 
 import { getProductCategories } from "@/lib/api/product/getProductCategories";
 import getProductModels from "@/lib/api/product/getProductModels";
+import { Suspense } from "react";
 
 import Tab from "@/components/ui/tab/Tab";
 
+import ProductListLoadingPage from "./loading";
 import styles from "./page.module.scss";
 
 export default async function ProductListPage({
@@ -28,11 +30,9 @@ export default async function ProductListPage({
     throw new Error("Categories not found");
   }
 
-  const models = await getProductModels(productId, categoryId);
-
   return (
     <main className={styles.page_wrap}>
-      {/* 카테고리 탭*/}
+      {/* 카테고리 탭 */}
       <Tab
         items={categories.data.map((category) => ({
           label: category.label,
@@ -40,21 +40,36 @@ export default async function ProductListPage({
         }))}
         activeValue={categoryId}
       />
-      {/* 제품 목록 */}
-      <ul
-        className={`${styles.product_list} ${models.data.length === 1 ? styles.one : models.data.length === 2 ? styles.two : models.data.length === 3 ? styles.three : ""}`}
-      >
-        {models.data.map((model) => (
-          <Link key={model.id} href={`/${productId}/${model.id}`}>
-            <li className={`${styles.product_item}`}>
-              <div className={styles.product_image}>
-                <Image src={model.image} alt={model.name} fill />
-              </div>
-              <div>{model.name}</div>
-            </li>
-          </Link>
-        ))}
-      </ul>
+      <Suspense key={categoryId} fallback={<ProductListLoadingPage />}>
+        <ProductList productId={productId} categoryId={categoryId} />
+      </Suspense>
     </main>
+  );
+}
+
+async function ProductList({
+  productId,
+  categoryId,
+}: {
+  productId: string;
+  categoryId: string;
+}) {
+  const models = await getProductModels(productId, categoryId);
+
+  return (
+    <ul
+      className={`${styles.product_list} ${models.data.length === 1 ? styles.one : models.data.length === 2 ? styles.two : models.data.length === 3 ? styles.three : ""}`}
+    >
+      {models.data.map((model) => (
+        <Link key={model.id} href={`/${productId}/${model.id}`}>
+          <li className={`${styles.product_item}`}>
+            <div className={styles.product_image}>
+              <Image src={model.image} alt={model.name} fill />
+            </div>
+            <div>{model.name}</div>
+          </li>
+        </Link>
+      ))}
+    </ul>
   );
 }
